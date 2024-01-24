a90ca4c95fcfce765016c81d0549fc50f7f8a2eb
Albert Sevy <albertstorres@gmail.com>
2024-01-24 00:38:58 -0300

    Finalizando o repositório da aula String/StringBuilder

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
-namespace StringBuilder
+using System.Text;//OBRIGATÓRIO PARA USO DO STRINGBUILDER.
+namespace Stringbuilder
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var texto = new StringBuilder();//CRIANDO VARIÁVEL DO TIPO STRINGBUILD
+            texto.Append("Este texto é um teste");//MUDA A FORMA DE ATRIBUIR VALORES A TEXTO.
+            texto.Append("Este texto é um teste");//CADA LINHA É COLADA JUNTO A ANTERIOR.
+            texto.Append("Este texto é um teste");
 
+            texto.ToString();//CONVERTENDO STRINGBUILD PARA STRING. TEXTOS MUITO GRANDES O WRITELINE NÃO CONSEGUE CONVERTER.
+
+            Console.WriteLine(texto);//RETORNA "Estetexto é um testeEste texto é um testeEste texto é um teste"
         }
     }
 }
